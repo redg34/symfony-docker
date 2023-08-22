55bbc7336c6d12712ad557d9819d436cedcad214
redg34 <regis.deroo@icloud.com>
2023-08-22 11:27:45 +0200

    expectException Guzzle

--- tests/Controller/ArticleTest.php
+++ tests/Controller/ArticleTest.php
@@ -5,21 +5,35 @@ namespace App\Tests\Controller;
 use PHPUnit\Framework\TestCase;
 use GuzzleHttp\Client;
 
-class ArticleTest extends TestCase
-{
-    public function testArticlesEndpoint(): void
-    {
-        $client = new Client([
+class ArticleTest extends TestCase {
+    private $_client;
+    
+    public function testArticlesEndpoint(): void {
+
+        
+        $response = $this->_client->get('articles');
+        
+        // Ensure a 200 Http status code after a GET
+        $this->assertEquals(200, $response->getStatusCode());
+        
+        // Ensure got Welcome to latte and code as content of response
+        $expectedResponseBody = 'Welcome to Latte and Code ';
+        $this->assertEquals($expectedResponseBody, (string) $response->getBody());
+    }
+    
+    public function testPostDoesRepondsWithNotFound(): void {
+        $this->expectException(\GuzzleHttp\Exception\ClientException::class);
+        $response = $this->_client->post('articles', []);
+        
+    }
+    
+    protected function setUp(): void {
+        $this->_client = new Client([
             'base_uri' => 'http://host.docker.internal/',
             'defaults' => [
                 'exceptions' => false
             ]
         ]);
-        
-        $response = $client->get('articles');
-        $this->assertEquals(200, $response->getStatusCode());
-
-        $expectedResponseBody = 'Welcome to Latte and Code ';
-        $this->assertEquals($expectedResponseBody, (string) $response->getBody());
     }
+    
 }
